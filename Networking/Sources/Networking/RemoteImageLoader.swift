28f52557443030c99caf4a140d1763d6a5e9aca7
Mohammad Gharari <gharary@me.com>
2022-12-22 09:10:55 +0100

    modifying image urls

--- Networking/Sources/Networking/RemoteImageLoader.swift
+++ Networking/Sources/Networking/RemoteImageLoader.swift
@@ -10,8 +10,8 @@ import Domain
 public class RemoteImageLoader: ImageLoader {
     public init() { }
     public func load(completion: @escaping (ImageLoader.Result) -> Void) {
-        let image = [ImageItem(id: UUID(), description: "any description", url: URL(string: "https://picsum.photos/200")!),
-                     ImageItem(id: UUID(), description: "any description", url: URL(string: "https://picsum.photos/200")!)]
+        let image = [ImageItem(id: UUID(), description: "any description", url: URL(string: "https://source.unsplash.com/user/c_v_r/200x200")!),
+                     ImageItem(id: UUID(), description: "any description", url: URL(string: "https://source.unsplash.com/user/c_v_r/200x200")!)]
         completion(.success(image))
     }
 }
