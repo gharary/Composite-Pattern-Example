c79dc5f9a16357905eab973b9e1df5884a2041a5
Mohammad Gharari <gharary@me.com>
2022-12-22 09:01:13 +0100

    move `ImageLoaderWithFallbackComposite` to production

--- ImageLoaderWithFallbackComposite/Sources/ImageLoaderWithFallbackComposite/ImageLoaderWithFallbackComposite.swift
+++ ImageLoaderWithFallbackComposite/Sources/ImageLoaderWithFallbackComposite/ImageLoaderWithFallbackComposite.swift
@@ -1,6 +1,29 @@
-public struct ImageLoaderWithFallbackComposite {
-    public private(set) var text = "Hello, World!"
+//
+//  File.swift
+//  
+//
+//
 
-    public init() {
+import Foundation
+import Domain
+
+class ImageLoaderWithFallbackComposite: ImageLoader {
+    private let primary: ImageLoader
+    private let fallback: ImageLoader
+    
+    init(primary: ImageLoader, fallback: ImageLoader) {
+        self.primary = primary
+        self.fallback = fallback
+    }
+    
+    func load(completion: @escaping (ImageLoader.Result) -> Void) {
+        primary.load { [weak self] result in
+            switch result {
+            case .success:
+                completion(result)
+            case .failure:
+                self?.fallback.load(completion: completion)
+            }
+        }
     }
 }
--- ImageLoaderWithFallbackComposite/Tests/ImageLoaderWithFallbackCompositeTests/ImageLoaderWithFallbackCompositeTests.swift
+++ ImageLoaderWithFallbackComposite/Tests/ImageLoaderWithFallbackCompositeTests/ImageLoaderWithFallbackCompositeTests.swift
@@ -2,26 +2,6 @@ import XCTest
 @testable import ImageLoaderWithFallbackComposite
 @testable import Domain
 
-class ImageLoaderWithFallbackComposite: ImageLoader {
-    private let primary: ImageLoader
-    private let fallback: ImageLoader
-    
-    init(primary: ImageLoader, fallback: ImageLoader) {
-        self.primary = primary
-        self.fallback = fallback
-    }
-    
-    func load(completion: @escaping (ImageLoader.Result) -> Void) {
-        primary.load { [weak self] result in
-            switch result {
-            case .success:
-                completion(result)
-            case .failure:
-                self?.fallback.load(completion: completion)
-            }
-        }
-    }
-}
 
 final class ImageLoaderWithFallbackCompositeTests: XCTestCase {
     
